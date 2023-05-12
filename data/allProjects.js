49fe6c65b5e52360dca1d23999e9bc0145bed98a
JstVex <heinhtetlulu14@gmail.com>
2023-05-12 20:17:29 +0630

    Create data for all the projects I have created

---
+++ data/allProjects.js
@@ -0,0 +1,62 @@
+export const allProjects = [
+    {
+        title: 'Youtube transcribe and Translator',
+        tech: ['Nextjs', 'TypeScript', 'OpenAI', 'Tailwind'],
+        githubUrl: 'https://github.com/JstVex/youtube_translator',
+        projectLink: ''
+    },
+    {
+        title: 'Architecture Portfolio',
+        tech: ['Nextjs', 'Framer Motion', 'Tailwind'],
+        githubUrl: 'https://github.com/JstVex/youtube_translator',
+        projectLink: ''
+    },
+    {
+        title: 'Food 2 Go',
+        tech: ['React', 'Vite', 'OpenAI', 'Tailwind'],
+        githubUrl: 'https://github.com/JstVex/youtube_translator',
+        projectLink: ''
+    },
+    {
+        title: 'Fitness Gym',
+        tech: ['Nextjs', 'Tailwind', 'DaisyUI'],
+        githubUrl: 'https://github.com/JstVex/youtube_translator',
+        projectLink: ''
+    },
+    {
+        title: 'Manager Portal for Soteria',
+        tech: ['Nextjs', 'Nodejs', 'Express', 'Mongodb'],
+        githubUrl: 'https://github.com/JstVex/youtube_translator',
+        projectLink: ''
+    },
+    {
+        title: 'Valentine',
+        tech: ['Nextjs', 'Framer Motion'],
+        githubUrl: 'https://github.com/JstVex/youtube_translator',
+        projectLink: ''
+    },
+    {
+        title: 'Soteria Version 1',
+        tech: ['Nextjs', 'Puppeteer', 'Nodejs', 'Express', 'Mongodb'],
+        githubUrl: 'https://github.com/JstVex/youtube_translator',
+        projectLink: ''
+    },
+    {
+        title: 'Task-it Out',
+        tech: ['React', 'Nodejs', 'Express', 'Mongodb'],
+        githubUrl: 'https://github.com/JstVex/youtube_translator',
+        projectLink: ''
+    },
+    {
+        title: '14th monthsary of us',
+        tech: ['HTML', 'CSS', 'Javascript'],
+        githubUrl: 'https://github.com/JstVex/youtube_translator',
+        projectLink: ''
+    },
+    {
+        title: 'Mg Charcoal lyy',
+        tech: ['HTML', 'CSS'],
+        githubUrl: 'https://github.com/JstVex/youtube_translator',
+        projectLink: ''
+    }
+]
